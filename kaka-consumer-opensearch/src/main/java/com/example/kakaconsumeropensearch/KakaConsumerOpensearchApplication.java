9d90f10788643384b517fccdba1289b3afe131ec
Kanishka <88154797+Kanishkavijay39@users.noreply.github.com>
2024-02-22 23:49:25 +0530

    Wikimedi 2 kafka project

---
+++ kaka-consumer-opensearch/src/main/java/com/example/kakaconsumeropensearch/KakaConsumerOpensearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaconsumeropensearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaConsumerOpensearchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KakaConsumerOpensearchApplication.class, args);
+  }
+
+}
---
+++ kaka-consumer-opensearch/src/test/java/com/example/kakaconsumeropensearch/KakaConsumerOpensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaconsumeropensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaConsumerOpensearchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
